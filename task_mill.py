dbbfe53142659697decc0c255a51bee7ef16cf32
Simon Meersschaut <simon.meersschaut@gmail.com>
2024-08-30 15:37:57 +0200

    test bug

--- task_mill.py
+++ task_mill.py
@@ -70,7 +70,7 @@ for task in task_data:
         data = rume_package.checks.check_sim_input(data)
       with open(folder+'/'+task['json_file'], 'w') as f_copy:
         json.dump(data, f_copy)
-      time.sleep(.001)
+      time.sleep(.1)
 
       # Combine .json and .imec into .dat for Ruthelde
       work_json_data = rume_package.combine(task['json_file'], 'data/'+txt_filename)
